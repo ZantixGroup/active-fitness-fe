8e139da18d0803e3ec86423a8fe82a53ed5577c3
Tapejika <Dmitrijs.ss@inbox.lv>
2024-04-19 20:49:07 +0300

    feat:UserFeedback

--- src/components/GroupClassesCard.vue
+++ src/components/GroupClassesCard.vue
@@ -1,5 +1,6 @@
 <template>
   <v-card
+      id="class-card"
       class="mx-auto"
       width="100%"
       hover
@@ -8,7 +9,7 @@
       <v-card-title style=" color: #FF4545">
         {{this.classes.title}}
       </v-card-title>
-
+      <p id="feedback-style" style="display: none; color:#7AE582;" >You have been accepted for the group class</p>
       <v-card-subtitle>
         {{this.classes.instructor.name}}
       </v-card-subtitle>
@@ -18,6 +19,17 @@
       {{this.classes.description}}
     </v-card-text>
 
+    <v-btn id="feedback-button" style="display:none; color:#7AE582;" type="button" @click="openFeedbackForm()" variant="text">leave feedback</v-btn>
+    <v-text-field
+        id="contet-field"
+        style="display:none;"
+        color="#FF4545"
+        label="Email Address"
+        type="email"
+        required
+        placeholder="Enter your email address"
+        variant="underlined"
+    ></v-text-field>
   </v-card>
 </template>
 <script>
@@ -28,5 +40,19 @@ export default {
       default: null,
     },
   },
+  mounted() {
+    this.axios.get(`/check_if_user_is_accepted/${this.classes.id}`).then(response => {
+      if (response.data.is_accepted === true) {
+        document.getElementById('class-card').style.border = "2px solid #7AE582";
+        document.getElementById('feedback-button').style.display = "block";
+        document.getElementById('feedback-style').style.display = "block";
+      }
+    })
+  },
+  methods: {
+    openFeedbackForm() {
+    document.getElementById('contet-field')
+    }
+  }
 };
 </script>
