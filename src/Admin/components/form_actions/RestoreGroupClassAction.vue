ef0f122eb140834e0af209bd82a956481a3345d9
Fleksis <markuss0303@gmail.com>
2024-05-20 01:07:18 +0300

    feat: added archive to group class

---
+++ src/Admin/components/form_actions/RestoreGroupClassAction.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-icon v-if="this.data?.deleted_at" icon="mdi-restore" @click="restore()"></v-icon>
+</template>
+
+<script>
+export default {
+  name: "RestoreGroupClassAction",
+  props: {
+    data: {
+      type: Object,
+      default: null,
+    },
+    name: {
+      type: String,
+      default: null,
+    },
+  },
+	mounted() {
+		console.log(this.data)
+	},
+	methods: {
+    restore() {
+			this.axios.get(`/group_class_archive_backup/${this.data.id}`).then(() => {
+				alert('Group class has been restored')
+				window.location.reload()
+			})
+    },
+  },
+};
+</script>
